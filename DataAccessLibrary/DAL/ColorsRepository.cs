fcbf167bf85944fd86f944a60486cbae0f2e7c58
Seleena Tandukar <seleena68@gmail.com>
2020-09-14 11:59:45 +1000

    UpdateRepo

---
+++ DataAccessLibrary/DAL/ColorsRepository.cs
@@ -0,0 +1,24 @@
+﻿using DataAccessLibrary.Interface;
+using SLT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SLT.DAL
+{
+    public class ColorsRepository : IColorsRepository
+    {
+
+        private SLTDbContext SLTDbContext;
+
+        public ColorsRepository(SLTDbContext context)
+        {
+            this.SLTDbContext = context;
+        }
+        public IEnumerable<Color> GetColors()
+        {
+            return SLTDbContext.Colors;
+        }
+    }
+}
---
+++ DataAccessLibrary/Interface/IColorsRepository.cs
@@ -0,0 +1,14 @@
+﻿using SLT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.Interface
+{
+    public interface IColorsRepository
+    {
+        IEnumerable<Color> GetColors();
+    }
+}
